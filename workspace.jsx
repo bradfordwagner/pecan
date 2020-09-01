712b2948705bb858767d7754b72bfbac46f940a3
bwagner <bwagner@blackrock.com>
2020-09-01 18:30:06 -0400

    cleanup type conversion

--- workspace.jsx
+++ workspace.jsx
@@ -12,27 +12,15 @@ export const command = dispatch => {
 }
 
 export const render = ({ workspace, spaces, display }) => {
-    if (!workspace) {
-        workspace = "0";
-    }
-    workspace = workspace.trim();
-    let workspaceIndex = parseInt(workspace)
-
-    let workspaces = (<div></div>);
-    if (spaces !== undefined) {
-       const spacesList = JSON.parse(spaces);
-       workspaces = spacesList.map((spaceId) => {
-         let spaceInt = parseInt(spaceId)
-         let style = {
-             "padding": "0.5em",
-         };
-         if (spaceInt === workspaceIndex) {
-             style.color = "#bd93f9"
-         }
-
-         return (<span style={style}>{spaceId}</span>)
-       })
-    }
+    let workspaces = spaces.map((space) => {
+      let style = {
+          "padding": "0.5em",
+      };
+      if (space === workspace) {
+          style.color = "#bd93f9"
+      }
+      return (<span style={style}>{space}</span>)
+    })
     return (<div class='screen'>
         <div class='pecanworkspace'>
             <span>D: { display } </span>
@@ -44,13 +32,24 @@ export const render = ({ workspace, spaces, display }) => {
 export const updateState = (event, previousState) => {
     switch (event.type) {
         case "workspace":
-            const { workspace } = event;
+            let { workspace } = event;
+            if (!workspace) {
+                workspace = "0";
+            }
+            workspace = workspace.trim();
+            workspace = parseInt(workspace);
             return {...previousState, workspace}
         case "display":
             const { display } = event;
             return {...previousState, display}
         case "spaces":
-            const { spaces } = event;
+            let { spaces } = event;
+            if (!spaces) {
+              spaces = [];
+            } else {
+              spaces = JSON.parse(spaces);
+              spaces = spaces.map(space => parseInt(space))
+            }
             return {...previousState, spaces}
         default:
             return previousState;

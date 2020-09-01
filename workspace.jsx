4144f19a12be25143e5df6d03fccec3e0bcfca16
bwagner <bwagner@blackrock.com>
2020-09-01 18:45:40 -0400

    ++ initial state

--- workspace.jsx
+++ workspace.jsx
@@ -3,7 +3,7 @@ import { run } from 'uebersicht'
 const ws ="bash pecan/scripts/ws";
 const spaces ="bash pecan/scripts/spaces";
 const display ="bash pecan/scripts/display";
-export const refreshFrequency = 500; // ms
+export const refreshFrequency = 1000; // ms
 
 export const command = dispatch => {
     run(ws).then(workspace => dispatch({ type: "workspace", workspace }))
@@ -19,39 +19,42 @@ export const render = ({ workspace, spaces, display }) => {
       if (space === workspace) {
           style.color = "#bd93f9"
       }
-      return (<span style={style}>{space}</span>)
+      return (<span key={space} style={style}>{space}</span>)
     })
-    return (<div class='screen'>
-        <div class='pecanworkspace'>
+    return (<div className='screen'>
+        <div className='pecanworkspace'>
             <span>D: { display } </span>
             <span>W: { workspaces }</span>
         </div>
     </div>);
 }
 
+export const initialState = {
+  workspace: 0,
+  spaces:    [0],
+  display:   0,
+}
+
 export const updateState = (event, previousState) => {
-    switch (event.type) {
-        case "workspace":
-            let { workspace } = event;
-            if (!workspace) {
-                workspace = "0";
-            }
-            workspace = workspace.trim();
-            workspace = parseInt(workspace);
-            return {...previousState, workspace}
-        case "display":
-            const { display } = event;
-            return {...previousState, display}
-        case "spaces":
-            let { spaces } = event;
-            if (!spaces) {
-              spaces = [0];
-            } else {
-              spaces = JSON.parse(spaces);
-              spaces = spaces.map(space => parseInt(space))
-            }
-            return {...previousState, spaces}
-        default:
-            return previousState;
-    }
+  switch (event.type) {
+    case "workspace":
+      let { workspace } = event;
+      workspace = workspace.trim();
+      workspace = parseInt(workspace);
+      return {...previousState, workspace}
+    case "display":
+      const { display } = event;
+      return {...previousState, display}
+    case "spaces":
+      let { spaces } = event;
+      if (!spaces) {
+        spaces = [0];
+      } else {
+        spaces = JSON.parse(spaces);
+        spaces = spaces.map(space => parseInt(space))
+      }
+      return {...previousState, spaces}
+    default:
+      return previousState;
+  }
 };
